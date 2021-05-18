6bfd21a0c630d5bf506cb5dbac55fb51d804183b
Bianca Duarte <bianca.du.raposo@gmail.com>
2021-05-17 22:12:35 -0300

    listando Repositórios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -5,11 +5,6 @@ import '../styles/repositories.scss';
 
 // https://api.github.com/orgs/rocketseat/repos
 
-const repository = {
-    name: 'unform',
-    description: 'Form in React',
-    link: 'https://github.com/unform/unform'
-}
 
 export function RepositoryList() {
     const [repositories, setRepositories] = useState([]);
@@ -25,11 +20,9 @@ export function RepositoryList() {
             <h1> Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     );
